0fcb2edef3802cceeee6abd5277abf8ab788bcc4
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-08-15 18:40:17 -0500

    Implement first responder for usernameField and passwordField

--- TestingTextFields/ViewController.swift
+++ TestingTextFields/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet private(set) var usernameField: UITextField!
+  @IBOutlet private(set) var passwordField: UITextField!
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    if textField === usernameField {
+      passwordField.becomeFirstResponder()
+      return false
+    }
+    
+    textField.resignFirstResponder()
+    
+    return false
   }
-
-
 }
 
--- TestingTextFieldsTests/TestingTextFieldsTests.swift
+++ TestingTextFieldsTests/TestingTextFieldsTests.swift
@@ -9,25 +9,64 @@ import XCTest
 @testable import TestingTextFields
 
 class TestingTextFieldsTests: XCTestCase {
+  func test_outlets_shouldBeConnected() {
+    let sut = makeSUT()
+        
+    XCTAssertNotNil(sut.usernameField)
+    XCTAssertNotNil(sut.passwordField)
+  }
+  
+  func test_usernameField_attributesShouldBeSet() {
+    let sut = makeSUT()
+    let textfield = sut.usernameField
+    
+    XCTAssertEqual(textfield?.textContentType, .username)
+    XCTAssertEqual(textfield?.autocorrectionType, .no)
+    XCTAssertEqual(textfield?.returnKeyType, .next)
+  }
+  
+  func test_passwordField_attributesShouldBeSet() {
+    let sut = makeSUT()
+    let textfield = sut.passwordField
+    
+    XCTAssertEqual(textfield?.textContentType, .password)
+    XCTAssertTrue(textfield!.isSecureTextEntry)
+    XCTAssertEqual(textfield?.returnKeyType, .go)
+  }
+  
+  func test_shouldReturn_changesFocusForUsernameAndPasswordFields() {
+    let sut = makeSUT()
+    putInViewHierarchy(sut)
+    
+    sut.usernameField.simulateReturnKeyPressed()
+    XCTAssertTrue(sut.passwordField.isFirstResponder)
+    
+    sut.passwordField.simulateReturnKeyPressed()
+    XCTAssertFalse(sut.usernameField.isFirstResponder)
+    XCTAssertFalse(sut.passwordField.isFirstResponder)
+  }
+  
+  // MARK: - Helpers
+  
+  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> ViewController {
+    let sb = UIStoryboard(name: "Main", bundle: nil)
+    let sut: ViewController = sb.instantiateViewController(identifier: String(describing: ViewController.self))
+    
+    sut.loadViewIfNeeded()
+    
+    return sut
+  }
+  
+  private func putInViewHierarchy(_ sut: ViewController) {
+    let window = UIWindow()
+    window.addSubview(sut.view)
+  }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+extension UITextField {
+  @discardableResult
+  func simulateReturnKeyPressed() -> Bool {
+    delegate?.textFieldShouldReturn?(self) ?? false
+  }
 }
